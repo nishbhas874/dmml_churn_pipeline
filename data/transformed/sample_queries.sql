daff3fe4fc8bf7ae00a5c39171b223884b66c121
nishbhas874 <nishanth.bhaskaran@gmail.com>
2025-08-22 20:09:41 +0530

    Data version 20250822_200941 - pipeline run

--- data/transformed/sample_queries.sql
+++ data/transformed/sample_queries.sql
@@ -1,5 +1,5 @@
 -- Sample SQL Queries for Churn Prediction Database
--- Generated on: 2025-08-22T20:08:10.757191
+-- Generated on: 2025-08-22T20:09:40.423881
 
 -- Get High Risk Customers
 SELECT c.customer_id, c.monthly_charges, cp.churn_probability
